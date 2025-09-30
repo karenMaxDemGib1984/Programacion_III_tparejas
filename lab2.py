258880e666586c888798f0857f2b17afbec8f213
Yimu21 <ulloa8435@gmail.com>
2025-09-30 09:58:57 -0600

    3,5,6

--- lab2.py
+++ lab2.py
@@ -1,5 +1,6 @@
 #Importando la libreria y renombrandola a pd 
 import pandas as pd
+import numpy as np
 
 #Poniendo el dataset en una variable
 dataset = pd.read_csv('data.csv')
@@ -7,6 +8,11 @@ dataset = pd.read_csv('data.csv')
 #Salidas
 print("Resumen de los datos:")
 print(dataset.describe())
+print(" ")
+
+# Identificar tipos de datos
+print("Tipos de datos en cada columna:")
+print(dataset.dtypes)
 print(" ")
 
 print("Primeras 5 filas de los datos:")
@@ -17,3 +23,18 @@ print(" ")
 print("Ultimas filas de los datos: ")
 print(dataset.tail())
 print(" ")
+
+print("Datos ordenados por la columna 'A' (ascendente):")
+print(dataset.sort_values(by='A').head())  # los primeros 5
+print(" ")
+
+print("Datos ordenados por la columna 'A' (descendente):")
+print(dataset.sort_values(by='A', ascending=False).head())
+print(" ")
+
+# Seleccionar una columna (ejemplo con columna 'B')
+columna = dataset['B']
+print("Medidas estadísticas sobre la columna 'B':")
+print("Media:", np.mean(columna))
+print("Mediana:", np.median(columna))
+print("Desviación estándar:", np.std(columna))
