6dea4faf4d88ace734de62d7b19c51c82b3f1dc0
karenMaxDemGib1984 <o4karenportillo@gmail.com>
2025-09-30 10:27:25 -0600

    comentarios

--- lab2.py
+++ lab2.py
@@ -1,5 +1,6 @@
 #Importando la libreria y renombrandola a pd 
 import pandas as pd
+#Importando la libreria numpy y renombrandola a np
 import numpy as np
 
 #Poniendo el dataset en una variable
@@ -15,19 +16,22 @@ print("Tipos de datos en cada columna:")
 print(dataset.dtypes)
 print(" ")
 
+#Salida de las primeras 5 filas
 print("Primeras 5 filas de los datos:")
 print(dataset.head())
 print(" ")
 
-
+#Salida de las ultimas 5 filas
 print("Ultimas filas de los datos: ")
 print(dataset.tail())
 print(" ")
 
+#Ordenando el dataset con la columna A de manera ascendente
 print("Datos ordenados por la columna 'A' (ascendente):")
 print(dataset.sort_values(by='A').head())  # los primeros 5
 print(" ")
 
+#Ordenando el dataset con la columna A de manera descendente
 print("Datos ordenados por la columna 'A' (descendente):")
 print(dataset.sort_values(by='A', ascending=False).head())
 print(" ")
